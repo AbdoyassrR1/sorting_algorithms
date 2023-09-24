c23c099bc23775fa225d91eeb05abcba6ac4f204
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-09-24 04:34:50 +0300

    fix code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,34 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n;
+	int swapped = 0;
 	int temp;
-	int swapped = 1;
+	size_t i;
+	size_t j;
 
-	if (!array || size < 2)
-		return;
-
-	n = size;
-	while (swapped)
+	if (array == NULL || size < 2)
 	{
-		swapped = 0;
-		for (i = 1; i < n; i++)
+		return;
+	}
+
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i - 1];
-				array[i - 1] = array[i];
-				array[i] = temp;
-				swapped = 1;
+
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
+				swapped++;
 			}
 		}
-		n--;
+		if (swapped == 0)
+		{
+			break;
+		}
 	}
 }
